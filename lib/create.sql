60f0380dc6d47910ee7c894a90aa1302462917cc
Edgar Irizarry <eirizarry15@yahoo.com>
2019-02-12 14:28:05 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -6,3 +6,12 @@ funding_goal INTEGER,
 start_date INTEGER,
 end_date INTEGER
 );
+
+CREATE TABLE user 
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER 
+);
+
+CREATE TABLE pledge
+id INTEGER PRIMARY KEY,
